d06d5b5aa1c6c0ffc8326b9bb24e1d25fb4752a3
Prince Sharma <prince786no.1@gmail.com>
2023-11-19 10:23:00 +0530

    rollDice and players

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function rollDice() {
     return Math.floor(Math.random() * 6) + 1;
 }
+
+
+
+let playerA = { name: "Player A", health: 100, strength: 5, attack: 10 };
+let playerB = { name: "Player B", health: 100, strength: 10, attack: 5 };
