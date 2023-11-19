6721caed30a410e3b9115d3bbc9c2eeff63a6f27
Prince Sharma <prince786no.1@gmail.com>
2023-11-19 10:54:30 +0530

    updated loops

--- index.js
+++ index.js
@@ -6,3 +6,28 @@ function rollDice() {
 
 let playerA = { name: "Player A", health: 100, strength: 5, attack: 10 };
 let playerB = { name: "Player B", health: 100, strength: 10, attack: 5 };
+
+
+let flag = true
+
+if (playerA.health > playerB.health) {
+    flag = false
+}
+
+console.log(playerA, playerB)
+
+while (playerA.health <= 0 || playerB.health <= 0) {
+    if (!flag) {
+        takeTurn(playerB, playerA);
+    } else {
+        takeTurn(playerA, playerB);
+    }
+
+    // if (playerB.health <= 0) break;
+
+    if (!flag) {
+        takeTurn(playerA, playerB);
+    } else {
+        takeTurn(playerB, playerA);
+    }
+}
